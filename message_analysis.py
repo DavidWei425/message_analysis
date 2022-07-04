b3df9fd4732b22f1718d93dfbcc5d0ecaebd0909
David Wei <bigwei0425@gmail.com>
2022-07-04 12:43:31 +0800

    calculate message len average

--- message_analysis.py
+++ message_analysis.py
@@ -3,3 +3,14 @@ with open('reviews.txt', 'r') as r:
 	for line in r:
 		data.append(line)
 print('檔案讀取完了,總共有', len(data), '筆資料')
+total = 0
+for x in range(0,len(data)):
+	total += len(data[x])
+	avg = total / len(data)
+print('每句留言的平均長度為 : ', avg)
+#或是以下寫法
+#sum_len = 0
+#for d in data:
+#	sum_len += len(d)
+#avg = sum_len / len(data)
+#print('留言的平均長度為 : ', avg)
